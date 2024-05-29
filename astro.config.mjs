74be46635a877a57e5bac30bbe2eceda1a14cd14
Michael O'Keefe <ok.coding355@gmail.com>
2024-05-29 17:09:27 -0400

    feat(garden): Finishing tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://okcoding.io",
+  integrations: [preact(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
+});
