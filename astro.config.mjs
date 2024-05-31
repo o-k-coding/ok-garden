a34db440d6a5dfc86caee0c11a3f9190a9575f6a
Michael O'Keefe <ok.coding355@gmail.com>
2024-05-30 23:24:19 -0400

    feat(garden): Adding update post for garden 05-30-2024

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,23 +8,22 @@ export default defineConfig({
   integrations: [starlight({
     title: 'OK Garden',
     favicon: '/favicon.ico',
-    sidebar: [{
-      label: 'About Me',
-      link: '/about'
-    },
-    // {
-    // 	label: 'Projects',
-    // 	autogenerate: { directory: 'projects' },
-    // },
-    // {
-    // 	label: 'Blog',
-    // 	autogenerate: { directory: 'blog' },
-    // },
-    {
-      label: 'Vennelos',
-      autogenerate: {
-        directory: 'vennelos'
-      }
-    }]
+    sidebar: [
+      {
+        label: 'About Me',
+        link: '/about'
+      },
+      {
+        label: 'Garden',
+        autogenerate: {
+          directory: 'garden'
+        }
+      },
+      {
+        label: 'Vennelos',
+        autogenerate: {
+          directory: 'vennelos'
+        }
+      }]
   }), preact()]
 });
